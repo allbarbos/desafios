6335a4ffbfdefa13a69fc9b24eb9de4ecaff348e
Allan Barbosa <allan.hb@outlook.com>
2018-02-07 22:48:27 -0200

    Implementação dos itens 1 à 7

---
+++ CaixaEletronico/Caixa/CaixaEletronico.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Caixa
+{
+  public class CaixaEletronico
+  {
+    public CaixaEletronico()
+    {
+      Cedulas = new Dictionary<string, int>
+      {
+        { "5", 0 },
+        { "10", 0 },
+        { "20", 0 },
+        { "50", 0 },
+        { "100", 0 }
+      };
+    }
+
+    public Dictionary<string, int> Cedulas { get; private set; }
+
+
+    public bool Deposito(string valor, int quantidade)
+    {
+      if (Cedulas.ContainsKey(valor) && quantidade > 0)
+      {
+        Cedulas[valor] += quantidade;
+
+        return true;
+      }
+
+      return false;
+    }
+
+    public bool VerificaLimite(int valor)
+    {
+      if (CalculaTotal() >= valor)
+      {
+        return true;
+      }
+
+      return false;
+    }
+
+    public int CalculaTotal()
+    {
+      return Cedulas.Sum(x => x.Value * Convert.ToInt32(x.Key));
+    }
+
+    public void Saldo()
+    {
+      Console.WriteLine("\n\n### Saldo ###\n");
+
+      foreach (var cedula in Cedulas)
+      {
+        Console.WriteLine($"Cédula: {cedula.Key} - Quantidade: {cedula.Value}\n");
+      }
+
+      Console.WriteLine($"Total: R$ {CalculaTotal()}\n");
+
+      Console.WriteLine("###########################\n");
+    }
+
+    public void Saque(int valor)
+    {
+      var valorCheio = valor;
+      var houveSaque = false;
+
+      Console.WriteLine("\n#### Saque ####\n");      
+
+      if (valor >= 100 && Cedulas["100"] > 0)
+      {
+        Debito(ref valor, "100");
+        houveSaque = true;
+      }
+
+      if (valor >= 50 && Cedulas["50"] > 0)
+      {
+        Debito(ref valor, "50");
+        houveSaque = true;
+      }                              
+                                     
+      if (valor >= 20 && Cedulas["20"] > 0)               
+      {
+        Debito(ref valor, "20");
+        houveSaque = true;
+      }                              
+                                     
+      if (valor >= 10 && Cedulas["10"] > 0)               
+      {
+        Debito(ref valor, "10");
+        houveSaque = true;
+      }
+
+      if (valor >= 5 && Cedulas["5"] > 0)
+      {
+        Debito(ref valor, "5");
+        houveSaque = true;
+      }
+
+      if (valor > 0 && houveSaque)
+      {
+        Console.WriteLine($"O caixa não possui notas para sacar o restante de R$ {valor}, portanto foi sacado apenas R$ {valorCheio - valor}");
+      }
+      else
+      {
+        Console.WriteLine($"O caixa não possui notas para sacar R$ {valor}");
+      }
+
+      Console.WriteLine("\n###############\n");
+    }
+
+
+    private void Debito(ref int valor, string cedula)
+    {
+      var qtdCedulasUtilizadas = valor / 100;
+      valor = valor % 100;
+      Cedulas[cedula] -= qtdCedulasUtilizadas;
+
+      Console.WriteLine($"{qtdCedulasUtilizadas} de {cedula}");
+    }
+  }
+}
---
+++ CaixaEletronico/Caixa/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Caixa
+{
+  class Program
+  {
+    public static bool ligado = true;
+
+    public static CaixaEletronico caixa = new CaixaEletronico();
+
+    static void Main(string[] args)
+    {
+      while (ligado)
+      {
+        Console.Clear();
+        Console.WriteLine($"Caixa Iniciado!\n");
+
+        EmOperacao();
+      }
+
+      Environment.Exit(1);
+    }
+
+    private static void EmOperacao()
+    {
+      Console.WriteLine($"1 - Deposito");
+      Console.WriteLine($"2 - Saldo");
+      Console.WriteLine($"3 - Saque");
+      Console.WriteLine($"0 - Desligar");
+
+      var opcao = Console.ReadKey();
+
+      if (opcao.Key.ToString().Equals("D0"))
+        ligado = false;
+
+      switch (opcao.Key.ToString())
+      {
+        case "D1":
+          {
+            Console.WriteLine($"\n\nQual o valor da cédula? (5 - 10 - 20 - 50 - 100)");
+            var valor = Console.ReadLine();
+
+            Console.WriteLine($"Qual a quantidade de cédula?");
+            var quantidade = Console.ReadLine();
+
+            int qtd;
+            if (int.TryParse(quantidade, out qtd))
+            {
+              if (caixa.Deposito(valor, qtd))
+              {
+                Console.WriteLine($"Deposito confirmado!");
+                Console.ReadKey();
+                break;
+              }
+            }
+
+            Console.WriteLine($"Deposito inválido!");
+            Console.ReadKey();
+            break;
+          }
+
+        case "D2":
+          {
+            caixa.Saldo();
+            Console.ReadKey();
+            break;
+          }
+
+        case "D3":
+          {
+            Console.WriteLine($"\n\nQual valor deseja sacar?");
+            var saque = Console.ReadLine();
+
+            int valor;
+            if (int.TryParse(saque, out valor))
+            {
+              if (caixa.VerificaLimite(valor))
+              {
+                caixa.Saque(valor);
+                Console.ReadKey();
+                break;
+              }
+
+              Console.WriteLine($"Caixa não possui R$ {valor} para saque - Limite disponível R$ {caixa.CalculaTotal()}");
+              Console.ReadKey();
+              break;
+            }
+
+            Console.WriteLine($"Saque inválido!");
+            Console.ReadKey();
+            break;
+          }
+      }
+    }
+  }
+}
