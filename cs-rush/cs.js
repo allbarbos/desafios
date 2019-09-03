7cecda3a0811a10fed683f543dc51c3d153885fb
Allan Barbosa <allan.barbosa@dextra-sw.com>
2019-09-03 16:04:03 -0300

    refatoração

--- cs-rush/cs.js
+++ cs-rush/cs.js
@@ -1,39 +1,43 @@
 const cssFactory = (css, vacantCss) => {
-  const vacantLess = css.filter(cssItem => !vacantCss.includes(cssItem[0]));
+  return css.reduce((anterior, atual) => {
+    if (!vacantCss.includes(atual[0])) {
+      anterior.push({
+        id: atual[0],
+        xp: atual[1],
+        clientes: []
+      })
+    }
 
-  return vacantLess.map(item => ({
-    id: item[0],
-    xp: item[1],
-    clientes: []
-  }));
-};
+    return anterior
+  }, [])
+}
 
 const customersFactory = customers =>
   customers.map(atual => ({
     id: atual[0],
     xp: atual[1],
     atendido: false
-  }));
+  }))
 
 const csOver = cssAvailable =>
   cssAvailable.reduce((anterior, atual) =>
     anterior.clientes.length > atual.clientes.length ? anterior : atual
-  );
+  )
 
 const csEqual = (cssAvailable, over) => {
-  let igual = false;
+  let igual = false
 
   cssAvailable.map(atual => {
     if (
       over.id !== atual.id &&
       over.clientes.length === atual.clientes.length
     ) {
-      igual = true;
+      igual = true
     }
-  });
+  })
 
-  return igual;
-};
+  return igual
+}
 
 const distribution = (clients, cssAvailable, maxDistribution) => {
   clients.map(customer => {
@@ -43,28 +47,28 @@ const distribution = (clients, cssAvailable, maxDistribution) => {
         customer.atendido === false &&
         func.clientes.length <= maxDistribution
       ) {
-        func.clientes.push(customer);
-        customer.atendido = true;
+        func.clientes.push(customer)
+        customer.atendido = true
       }
-    });
-  });
-};
+    })
+  })
+}
 
 const csRush = (n, m, css, customers, vacantCss) => {
-  const clients = customersFactory(customers);
-  const customersTotal = m;
+  const clients = customersFactory(customers)
+  const customersTotal = m
 
-  const cssAvailable = cssFactory(css, vacantCss);
-  const cssTotal = cssAvailable.length;
+  const cssAvailable = cssFactory(css, vacantCss)
+  const cssTotal = cssAvailable.length
 
-  const maxDistribution = customersTotal / cssTotal;
+  const maxDistribution = customersTotal / cssTotal
 
-  distribution(clients, cssAvailable, maxDistribution);
+  distribution(clients, cssAvailable, maxDistribution)
 
-  const over = csOver(cssAvailable);
-  const equal = csEqual(cssAvailable, over);
+  const over = csOver(cssAvailable)
+  const equal = csEqual(cssAvailable, over)
 
-  return equal ? 0 : over.id;
-};
+  return equal ? 0 : over.id
+}
 
-module.exports = csRush;
+module.exports = csRush
--- cs-rush/tests/cs.test.js
+++ cs-rush/tests/cs.test.js
@@ -1,27 +1,27 @@
-const csRush = require("../cs");
+const csRush = require('../cs')
 
-describe("CS Rush", () => {
-  it("Deve realizar a distribuição e sobrecarregar o funcionário de ID 1", async () => {
-    const csTotal = 4;
-    const customersTotal = 6;
-    const css = [[1, 60], [2, 20], [3, 95], [4, 75]];
-    const customers = [[1, 90], [2, 20], [3, 70], [4, 40], [5, 60], [6, 10]];
-    const vacantCss = [2, 4];
+describe('CS Rush', () => {
+  it('Deve realizar a distribuição e sobrecarregar o funcionário de ID 1', async () => {
+    const csTotal = 4
+    const customersTotal = 6
+    const css = [[1, 60], [2, 20], [3, 95], [4, 75]]
+    const customers = [[1, 90], [2, 20], [3, 70], [4, 40], [5, 60], [6, 10]]
+    const vacantCss = [2, 4]
 
-    const result = csRush(csTotal, customersTotal, css, customers, vacantCss);
+    const result = csRush(csTotal, customersTotal, css, customers, vacantCss)
 
-    expect(result).toBe(1);
-  });
+    expect(result).toBe(1)
+  })
 
-  it("Deve realizar a distribuição equalizada e retornar 0", async () => {
-    const csTotal = 4;
-    const customersTotal = 6;
-    const css = [[1, 60], [2, 20], [3, 95], [4, 75]];
-    const customers = [[1, 90], [2, 20], [3, 70], [4, 40], [5, 60], [6, 10]];
-    const vacantCss = [];
+  it('Deve realizar a distribuição equalizada e retornar 0', async () => {
+    const csTotal = 4
+    const customersTotal = 6
+    const css = [[1, 60], [2, 20], [3, 95], [4, 75]]
+    const customers = [[1, 90], [2, 20], [3, 70], [4, 40], [5, 60], [6, 10]]
+    const vacantCss = []
 
-    const result = csRush(csTotal, customersTotal, css, customers, vacantCss);
+    const result = csRush(csTotal, customersTotal, css, customers, vacantCss)
 
-    expect(result).toBe(0);
-  });
-});
+    expect(result).toBe(0)
+  })
+})
